f82e85dd7f4875ffbe3c87a302cdda4e38f022f9
Gnivrianno <Geraldy.nivrianno@gmail.com>
2022-05-17 11:43:14 +0700

    Add files via upload

---
+++ app/src/main/java/com/asd/android/doctorapp/Data.java
@@ -0,0 +1,17 @@
+package com.asd.android.doctorapp;
+
+/**
+ * Created by Mantan on 7/29/2016.
+ */
+public class Data {
+    public String title;
+    public String description;
+    public int imageId;
+
+    Data(String title, String description, int imageId) {
+        this.title = title;
+        this.description = description;
+        this.imageId = imageId;
+    }
+
+}
